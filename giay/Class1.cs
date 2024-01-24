dc1bdc22a08c5567c09d23f256405bb632d9aecc
ADMIN <ADMIN@CHOP>
2024-01-24 13:19:08 +0700

    Add project files.

---
+++ giay/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace giay
+{
+    internal class Giay
+    {
+        public Giay(int giayID, string tenGiay, string thuongHieu, int size, string mauSac, int gia, int tonKho)
+        {
+            this.giayID = giayID;
+            this.tenGiay = tenGiay;
+            this.thuongHieu = thuongHieu;
+            this.size = size;
+            this.mauSac = mauSac;
+            Gia = gia;
+            this.tonKho = tonKho;
+        }
+        public void cn1()
+        {
+            
+        }
+        public int giayID { get; set; }
+        public string tenGiay { get; set; }
+        public string thuongHieu { get; set;}
+        public double size { get; set; }
+        public string mauSac { get; set; } 
+        public int Gia { get; set; }
+        public int tonKho { get; set;}
+        
+    }
+}
---
+++ giay/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace giay
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+            Console.ReadKey();
+
+        }
+        static void Menu() {
+            int chon;
+            ArrayList Shoes = new ArrayList();
+
+            do {
+
+                Console.WriteLine("---------------Cua hang ban giay NET101---------------");
+                Console.WriteLine("1.Them moi mot doi giay");
+                Console.WriteLine("2.Danh sach Giay");
+                Console.WriteLine("3.Mua Giay");
+                Console.WriteLine("4.Thoat");
+                Console.WriteLine("Chon tu 1-4");
+
+
+                chon = Convert.ToInt32(Console.ReadLine());
+                switch (chon)
+                {
+                    case 1:
+                        break;
+
+
+                    case 2:
+                       
+                        break;
+
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    default: Console.WriteLine("Vui long nhap lai!!");
+                        break;
+
+                }
+            } while (chon != 4);
+        }
+        static void
+            
+        }
+    }
+
